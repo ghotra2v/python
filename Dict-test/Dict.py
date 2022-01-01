0e1a92c723ca85a77442e8e6c855f370c245454d
varinder Ghotra <ghotra3v@gmail.com>
2021-12-31 20:53:03 -0600

    master

---
+++ Dict-test/Dict.py
@@ -0,0 +1,10 @@
+dict = {
+    "Varinder": "24-08-2021",
+    "Mandeep": "24-08-2021",
+    "Lakhwant": "10-01-2021",
+    "Zorawar": "04-01-2021",
+}
+
+
+name = input("Enter your name: ")
+print(dict[name])
